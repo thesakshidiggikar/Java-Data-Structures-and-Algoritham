233430a02158e91fa1a63c674d8a476d8df941de
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-20 21:53:25 +0530

    loops in java

--- loops.java
+++ loops.java
@@ -1,3 +1,30 @@
+//for loop  :-  (Start ; step ; stop ) or ( initialisation ; conditiona ; updation)
+//while loop  :- 
+// do while loop
+
 public class loops {
-    
+    public static void main(String args[]) {
+
+        // ---------------------------------for loop ------------------------------------------
+        // for(int counter = 0; counter < 4; counter=counter +1 ){
+        // System.out.println("*");
+        // }
+
+
+        // ---------------------------------while loop ------------------------------------------
+        // int counter = 0;
+        // while (counter < 10){
+        //     System.out.println(counter);
+        //     counter=counter+1;
+        // }
+
+        // --------------------------------- do while loop ------------------------------------------
+
+        int counter = 0;
+        do{
+            System.out.println(counter);
+            counter=counter+1;
+        }
+        while (counter < 12);
+    }
 }
---
+++ pattern/hollo_rectangle.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class hollo_rectangle {
+    public static void main()_{
+        int a = 1;
+        int b = 5;
+
+        for (int i = 0 ; i<a ; i ++ ){
+            
+        }
+    }
+
+}
