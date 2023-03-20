e2aace71cff7154c80ef6bac81d669d0f480d76c
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-20 22:54:34 +0530

    ptterns in java

---
+++ first_n_natural_numbers.java
@@ -0,0 +1,11 @@
+public class first_n_natural_numbers {
+    public static void main(String args[]){
+        int n = 4;
+        int sum = 0 ;
+        for(int i = 0 ; i <= n ; i ++ ){
+            sum=sum + i;
+        }
+        System.out.println(sum);
+    }
+    
+}
--- pattern/hollo_rectangle.java
+++ pattern/hollo_rectangle.java
@@ -1,13 +1,32 @@
 package pattern;
 
 public class hollo_rectangle {
-    public static void main(){
-        int a = 1;
+    /**
+     * 
+     */
+    public static void main(String args[]){
+        int a = 4;
         int b = 5;
 
-        for (int i = 0 ; i<a ; i ++ ){
-            
+        for (int i = 1 ; i <= a ; i ++ ){
+            for (int j = 1 ; j <= b ; j ++ ){
+
+                 if ( i==1 || j==1 || i==a || j == b){
+                    System.out.print(" *");
+                 }
+                 else{
+                    System.out.print("  ");
+                 }
+            }
+            System.out.println();
         }
     }
 
 }
+
+
+//output
+//        * * * * *
+//        *       *
+//        *       *
+//        * * * * *
