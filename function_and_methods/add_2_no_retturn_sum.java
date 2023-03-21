774dd4c60acea7c3b84218e133f84e1f3a24050d
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-21 09:47:13 +0530

    updated

---
+++ function_and_methods/add_2_no_retturn_sum.java
@@ -0,0 +1,11 @@
+package function_and_methods;
+
+public class add_2_no_retturn_sum {
+
+    public static Additon(sum){
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int a = 
+    }
+}
