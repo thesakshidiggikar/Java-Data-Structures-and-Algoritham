16a992c40d9b7a651c746294c5e8376e649d02b1
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-23 23:52:13 +0530

    Sample ques for 2D array

--- Arrays/sample_ques.java
+++ Arrays/sample_ques.java
@@ -1,7 +1,19 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class sample_ques {
     public static void main(String args[]){
-        
+        Scanner sc= new Scanner(System.in);
+        int rows =sc.nextInt();
+        int col= sc.nextInt();
+
+        int number[][]=new int[rows][col]
+
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<col; j++){
+                number[i][j]=sc.nextInt();
+            }
+        }
     }
 }
