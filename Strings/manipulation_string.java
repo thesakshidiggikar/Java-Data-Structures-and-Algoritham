a9bc2d2c72df5b1f37e7b695c9f668edbf253b03
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-24 23:22:52 +0530

    update

--- Strings/manipulation_string.java
+++ Strings/manipulation_string.java
@@ -1,5 +1,15 @@
 package Strings;
 
 public class manipulation_string {
-    
+    public static void main(String args[]) {
+        // complarisons
+        String name1 = "Sakshi";
+        String name2 = "Anu";
+
+        if (name1.compareTo(name1 name2) ) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
