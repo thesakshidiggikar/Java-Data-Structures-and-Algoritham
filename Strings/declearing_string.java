0d73cb446756c588cd47cf126157ee769d48dbc0
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-24 21:43:07 +0530

    String  deceleration and user input

--- Strings/declearing_string.java
+++ Strings/declearing_string.java
@@ -4,8 +4,27 @@ import java.util.*;
 
 public class declearing_string {
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
-        System.out.println(a);
+        // Scanner sc=new Scanner(System.in);
+        // String a=sc.nextLine();
+        // System.out.println(a);
+
+        // String name= "Sakshi Diggikar";
+        // System.out.println(name);
+
+        //concatenation
+        String name1= "anushka";
+        String name2= "diggikar";
+        String fullname= name1+name2;
+        //System.out.println(fullname.length());
+
+
+        //string indexing starts from 1 to n.
+        //print all character using charAt
+        // for (int i = 0; i <fullname.length();i++){
+        //     System.out.println(fullname.charAt(i));
+        // }
+
+        
+
     }
 }
