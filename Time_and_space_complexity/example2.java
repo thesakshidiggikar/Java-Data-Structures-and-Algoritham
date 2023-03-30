5144c7bc6ad349527f46beba40f2343255c914c0
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-03-30 19:04:57 +0530

    updated

--- Time_and_space_complexity/example2.java
+++ Time_and_space_complexity/example2.java
@@ -19,6 +19,9 @@ public class example2 {
         for (int j=0; j<= b ; j++){
             System.out.println("&");
         }
+        for (int j=0; j<= b ; j++){
+            System.out.println("&");
+        }
         
     }
 }
