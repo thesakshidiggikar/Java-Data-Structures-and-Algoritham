2f2761dea0906a68b68b6e1cb97de54e09b054f9
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-01 22:23:54 +0530

    updated

--- Arrays/sample_ques.java
+++ Arrays/sample_ques.java
@@ -1,21 +1,21 @@
-package Arrays;
+// package Arrays;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class sample_ques {
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        int rows =sc.nextInt();
-        int col= sc.nextInt();
+// public class sample_ques {
+//     public static void main(String args[]){
+//         Scanner sc= new Scanner(System.in);
+//         int rows =sc.nextInt();
+//         int col= sc.nextInt();
 
-        int number[][]=new int[rows][col];
-        int x= sc.nextInt();
-        for (int i=0; i<rows; i++){
-            for (int j=0; j<col; j++){
-               if(number[i][j]==x){
-                System.out.println("x is fouond at "+  " ");
-               }
-            }
-        }
-    }
-}
+//         int number[][]=new int[rows][col];
+//         int x= sc.nextInt();
+//         for (int i=0; i<rows; i++){
+//             for (int j=0; j<col; j++){
+//                if(number[i][j]==x){
+//                 System.out.println("x is fouond at "+  " ");
+//                }
+//             }
+//         }
+//     }
+// }
