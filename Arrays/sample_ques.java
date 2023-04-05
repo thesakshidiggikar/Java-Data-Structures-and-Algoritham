1575d155da3eadb3b976c38714f982c4175d4973
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-05 22:55:02 +0530

    Updated

--- Arrays/sample_ques.java
+++ Arrays/sample_ques.java
@@ -20,3 +20,25 @@ public class sample_ques {
     }
 }
 
+package Arrays;
+
+import java.util.Scanner;
+
+public class sample_ques {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows =sc.nextInt();
+        int col= sc.nextInt();
+
+        int number[][]=new int[rows][col];
+        int x= sc.nextInt();
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<col; j++){
+               if(number[i][j]==x){
+                System.out.println("x is fouond at "+  " ");
+               }
+            }
+        }
+    }
+}
+
