d5386aa8ca23d8144eef8ecf83f42e839407978f
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-08 21:13:10 +0530

    Updated

--- Strings/manipulation_string.java
+++ Strings/manipulation_string.java
@@ -27,6 +27,32 @@ public class manipulation_string {
         //string im immutable
     }
 }
+package Strings;
+
+public class manipulation_string {
+    public static void main(String args[]) {
+        // complarisons
+        String name1 = "Sakshi";
+        String name2 = "Anu";
 
 
+        //1 sl>s2: +ve value
+        //2 s2:0
+        //3 s1<s2: -ve value
 
+
+        // if (name1.compareTo(name2) ==0) {
+        //     System.out.println("Yes");
+        // }
+        if (name1==name2) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("NO");
+        }
+
+
+        //substring
+
+        //string im immutable
+    }
+}
