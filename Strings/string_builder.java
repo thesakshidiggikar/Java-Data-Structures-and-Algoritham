6ff11cf02f5e07c4a8673066941de077c7752b8c
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-12 23:10:07 +0530

    Updated

--- Strings/string_builder.java
+++ Strings/string_builder.java
@@ -30,4 +30,13 @@ public class string_builder {
     //String statement 
     }
 }
+package Strings;
+
+public class string_builder {
+    //string immutable
+    public static void main(String args[]){
+    //String statement 
+    }
+}
+
 
