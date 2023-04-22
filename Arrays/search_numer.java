9bf4c7d102de183011e1f5c28d720adc56866e71
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-22 19:46:00 +0530

    Updated

--- Arrays/search_numer.java
+++ Arrays/search_numer.java
@@ -129,3 +129,134 @@ public class search_numer {
 
 //array length
 
+package Arrays;
+
+import java.util.Scanner;
+
+public class search_numer {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int array[] = new int[size];
+
+        for (int i = 0 ; i <size ; i ++){
+            array[i] = sc.nextInt();
+        }
+
+        int j = sc.nextInt();
+        
+        for(int i =0 ; i <array.length ; i ++){
+            if (array[i]==j){
+                System.out.println("X found at index:  " + j);
+            }
+        }
+        
+
+
+        
+        
+    }
+}
+
+
+
+//array length
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class search_numer {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int array[] = new int[size];
+
+        for (int i = 0 ; i <size ; i ++){
+            array[i] = sc.nextInt();
+        }
+
+        int j = sc.nextInt();
+        
+        for(int i =0 ; i <array.length ; i ++){
+            if (array[i]==j){
+                System.out.println("X found at index:  " + j);
+            }
+        }
+        
+
+
+        
+        
+    }
+}
+
+
+
+//array length
+package Arrays;
+
+import java.util.Scanner;
+
+public class search_numer {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int array[] = new int[size];
+
+        for (int i = 0 ; i <size ; i ++){
+            array[i] = sc.nextInt();
+        }
+
+        int j = sc.nextInt();
+        
+        for(int i =0 ; i <array.length ; i ++){
+            if (array[i]==j){
+                System.out.println("X found at index:  " + j);
+            }
+        }
+        
+
+
+        
+        
+    }
+}
+
+
+
+//array length
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class search_numer {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int array[] = new int[size];
+
+        for (int i = 0 ; i <size ; i ++){
+            array[i] = sc.nextInt();
+        }
+
+        int j = sc.nextInt();
+        
+        for(int i =0 ; i <array.length ; i ++){
+            if (array[i]==j){
+                System.out.println("X found at index:  " + j);
+            }
+        }
+        
+
+
+        
+        
+    }
+}
+
+
+
+//array length
+
