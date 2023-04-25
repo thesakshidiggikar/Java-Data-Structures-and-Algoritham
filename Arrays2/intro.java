d620a3fb05a31fcbb22eddc06f210da9432e12bf
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-04-25 23:39:17 +0530

    Updated

--- Arrays2/intro.java
+++ Arrays2/intro.java
@@ -65,3 +65,17 @@ public class intro{
     }
 }
 
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+
