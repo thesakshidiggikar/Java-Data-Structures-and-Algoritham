829f674ad4b8677f4520b767721246cf96e76e53
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-02 10:50:21 +0530

    updated

--- Arrays2/intro.java
+++ Arrays2/intro.java
@@ -259,6 +259,73 @@ public class intro{
 
     }
 }
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+//2D array
+import java.utils.*;
+public class intro{
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+
+        int [] []  numbers = new int[rows][cols];
+
+    }
+}
+
+
 
 
 
