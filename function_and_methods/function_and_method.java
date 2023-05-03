ef84dd6b840e56cbb8227f364f87f40d05d43ed9
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-03 09:54:52 +0530

    Updated

--- function_and_methods/function_and_method.java
+++ function_and_methods/function_and_method.java
@@ -62,3 +62,25 @@ public class function_and_method {
     }
 
 }
+package function_and_methods;
+
+import java.util.Scanner;
+
+public class function_and_method {
+
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+
+    }
+
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name);
+    }
+
+}
+
