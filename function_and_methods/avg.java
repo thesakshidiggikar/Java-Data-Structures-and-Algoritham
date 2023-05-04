1e835deaec8037afbb72c8843eb138b2a05e0651
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-04 20:52:37 +0530

    Updated

--- function_and_methods/avg.java
+++ function_and_methods/avg.java
@@ -22,3 +22,27 @@ public class avg {
     Average(a, b, c);
     }
 }
+
+// Enter 3 number fomr user and make a function to print their average
+package function_and_methods;
+
+import java.rmi.StubNotFoundException;
+import java.util.Scanner;
+
+public class avg {
+    public static void Average(int a , int b , int c ){
+        int avg = a + b + c /3;
+
+        System.out.println(avg);
+        return;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc. nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+    Average(a, b, c);
+    }
+}
