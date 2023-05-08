cc72316b3820b66b01f8c87b9f933f2faa58f3fb
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-08 19:18:10 +0530

    Updated

--- function_and_methods/factorial.java
+++ function_and_methods/factorial.java
@@ -166,3 +166,59 @@ public class factorial {
         fact(n);
     }
 }
+package function_and_methods;
+
+import java.util.Scanner;
+
+public class factorial {
+
+    public static void fact(int n) {
+        if (n < 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+        int factorial = 1;
+
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        fact(n);
+    }
+}
+package function_and_methods;
+
+import java.util.Scanner;
+
+public class factorial {
+
+    public static void fact(int n) {
+        if (n < 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+        int factorial = 1;
+
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        fact(n);
+    }
+}
