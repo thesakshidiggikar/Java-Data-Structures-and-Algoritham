280c05a53d5f0d7f768f7c329891accc68c1bbc2
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-11 20:13:52 +0530

    Updated

--- function_and_methods/fucntions.java
+++ function_and_methods/fucntions.java
@@ -32,3 +32,21 @@ public class fucntions {
     
     
 }
+package function_and_methods;
+import java.util.*;
+public class fucntions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+
+        printMyName(name);
+    }
+    
+    
+}
+
