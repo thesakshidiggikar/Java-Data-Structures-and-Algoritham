c41d8830ef3b16db2726f21c2b0ac2914bb013aa
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-13 11:43:18 +0530

    Updated

--- function_and_methods/fucntions.java
+++ function_and_methods/fucntions.java
@@ -69,3 +69,74 @@ public class fucntions {
     
 }
 
+package function_and_methods;
+import java.util.*;
+public class fucntions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+
+        printMyName(name);
+    }
+    
+    
+}
+
+package function_and_methods;
+import java.util.*;
+public class fucntions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+
+        printMyName(name);
+    }
+    
+    
+}
+package function_and_methods;
+import java.util.*;
+public class fucntions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+
+        printMyName(name);
+    }
+    
+    
+}
+
+package function_and_methods;
+import java.util.*;
+public class fucntions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String name= sc.next();
+
+        printMyName(name);
+    }
+    
+    
+}
+
