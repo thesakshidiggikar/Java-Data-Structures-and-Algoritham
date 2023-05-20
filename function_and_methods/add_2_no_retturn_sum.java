693ebf600331254c35966c840f2c6ecca4cdeacf
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-20 12:36:57 +0530

    Updated

--- function_and_methods/add_2_no_retturn_sum.java
+++ function_and_methods/add_2_no_retturn_sum.java
@@ -98,3 +98,23 @@ public class add_2_no_retturn_sum {
         System.out.println( sum);
     }
 }
+package function_and_methods;
+
+import java.util.Scanner;
+
+public class add_2_no_retturn_sum {
+
+    public static int Addition(int a , int b){
+       int  sum = a + b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc=  new Scanner(System.in);
+        int a  = sc.nextInt();
+        int b = sc.nextInt();
+        
+
+        int sum= Addition(a,b);
+        System.out.println( sum);
+    }
+}
