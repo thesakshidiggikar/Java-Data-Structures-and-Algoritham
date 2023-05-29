f3de85b900b8cc1553371bf5edfc58bf29c254a6
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-05-29 19:35:30 +0530

    Updated

--- String_builder/string_builder.java
+++ String_builder/string_builder.java
@@ -10,4 +10,16 @@ public static Strings {
         //char at index 0
         System.out.println(sb.charAt(0));
     }
-}z
+}
+import java.util.*;
+
+
+public static Strings {
+    public static void  main(String args[]){
+        StringBuilder sb= new StringBuilder("tony");
+        System.out.println(sb);
+
+        //char at index 0
+        System.out.println(sb.charAt(0));
+    }
+}
