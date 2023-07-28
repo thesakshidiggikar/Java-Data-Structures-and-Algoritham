6c394e53c4727bd12ac2a3142bc8acbdaeddaf8f
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-07-28 19:12:13 +0530

    Updated

--- String_builder/StringCharIndex.java
+++ String_builder/StringCharIndex.java
@@ -88,4 +88,34 @@ public static StringCharIndex {
         String a = sb new String("Max");
         
 }
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
+}public static StringCharIndex {
+    public static void main(String argsp[]){
+        String a = sb new String("Max");
+        
+}
 }
