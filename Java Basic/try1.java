f4e35758910d61064f3de6e18204b5a505bfb2f9
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-08-15 20:36:46 +0530

    Updated

--- Java Basic/try1.java
+++ Java Basic/try1.java
@@ -75,3 +75,14 @@ public class first_n_natural_numbers {
     
 }
 
+public class first_n_natural_numbers {
+    public static void main(String args[]){
+        int n = 4;
+        int sum = 0 ;
+        for(int i = 0 ; i <= n ; i ++ ){
+            sum=sum + i;
+        }
+        System.out.println(sum);
+    }
+    
+}
