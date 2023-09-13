57a007b9a11a6e2e0e227c291fd3124a4c9b78a5
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-09-13 19:59:03 +0530

    Updated

--- Arrays/arrays2d.java
+++ Arrays/arrays2d.java
@@ -97,3 +97,36 @@ public class arrays2d {
       
    }
 }
+package Arrays;
+
+import java.util.*;;
+
+public class arrays2d {
+   public static void main(String args[]) {
+      // way to write 2D arrays
+      // type [][] arrayName = new type [row] [column];
+      // int [] [] number = new int [3][4];
+
+      Scanner sc = new Scanner(System.in);
+      int rows = sc.nextInt();
+      int col = sc.nextInt();
+
+      // initializating 2D array
+      int number[][] = new int[rows][col];
+
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < col; j++) {
+            number[i][j] = sc.nextInt();
+         }
+      }
+
+      //output
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < col; j++) {
+            System.out.print(number[i][j]+ " ");
+         }
+         System.out.println();
+      }
+      
+   }
+}
