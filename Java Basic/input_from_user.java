7ac32ae1f62cfa5960974631422e1160c488197d
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-09-26 19:06:22 +0530

    Updated

--- Java Basic/input_from_user.java
+++ Java Basic/input_from_user.java
@@ -174,3 +174,25 @@ public class input_from_user {
         //nextLine()
     }
 }
+import java.util.*;  //package
+
+
+public class input_from_user {
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner(System.in);  // this object 
+        // for input we will use "System.in"
+
+        // scanner will use "next" function to store input from the user
+        String name = sc.nextLine(); // took input frm user 
+        // "next" will use only one token
+
+        String name1 = sc.nextLine();
+        // to use entire line we will use nextLine
+        System.out.println(name);
+
+
+        //next()
+        //nextLine()
+    }
+}
