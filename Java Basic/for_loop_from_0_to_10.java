a1faecf91d68cf39f48172c8788bca0c0e6702a2
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-09-29 16:56:26 +0530

    Updated

--- Java Basic/for_loop_from_0_to_10.java
+++ Java Basic/for_loop_from_0_to_10.java
@@ -1,6 +1,6 @@
 public class for_loop_from_0_to_10 {
     public static void main(String args[]){
-        // ques. print number frmo 0 to 10
+        // ques. print number from 0 to 10
         for (int counter = 0; counter < 11; counter++) {
             System.out.println(counter);
         }
