488aa9361646d05314159da7fd1a5e0ae880474e
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-10-17 22:37:55 +0530

    Updated

--- pattern/solid_rectangle.java
+++ pattern/solid_rectangle.java
@@ -102,4 +102,25 @@ public class solid_rectangle {
 //       *  *  *  *  *
 //       *  *  *  *  *
 //       *  *  *  *  *package pattern;
+package pattern;
+
+public class solid_rectangle {
+    public static void main(String arga[]){
+        for (int i=1; i <= 4 ; i++)//rows 
+        { 
+            for( int j =1; j<=5 ; j++) // columns
+            {
+                System.out.print("  *");
+            }
+        System.out.println();
+    }
+}
+}
+
+// -----------------------output :-----------------------
+//       *  *  *  *  *
+//       *  *  *  *  *
+//       *  *  *  *  *
+//       *  *  *  *  *package pattern;
+
 
