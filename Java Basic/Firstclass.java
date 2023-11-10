833e07668eeeaf64617b8b7a733e26c931ff81f6
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-11-10 22:43:30 +0530

    Updated

--- Java Basic/Firstclass.java
+++ Java Basic/Firstclass.java
@@ -96,4 +96,18 @@ class Firstclass{ // " Big Entity"
     }
 
 
+}class Firstclass{ // " Big Entity" 
+    //
+    public static void main (String args []){  // "void main" is Function 
+        System.out.println("Hello World");
+    }
+
+
+}class Firstclass{ // " Big Entity" 
+    //
+    public static void main (String args []){  // "void main" is Function 
+        System.out.println("Hello World");
+    }
+
+
 }
