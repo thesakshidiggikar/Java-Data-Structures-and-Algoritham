71d31ab4d034e6ad96319ece02b64c12cf189a7b
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-01-12 14:25:26 +0530

    Updated

--- Java Basic/Firstclass.java
+++ Java Basic/Firstclass.java
@@ -64,7 +64,7 @@ class Firstclass{ // " Big Entity"
         System.out.println("Hello World");
     }
 
-}class Firstclass{ // " Big Entity" 
+}class Firstclass{ // " Big Entity"
     //
     public static void main (String args []){  // "void main" is Function 
         System.out.println("Hello World");
